c1a4400f885201f0978b13848dc73585c39c86a6
Ryan Blackman <rblackman24@gmail.com>
2014-12-08 17:50:12 -0600

    initial commit

---
+++ highlight.js
@@ -0,0 +1,27 @@
+ko.bindingHandlers.highlight = {
+	init: function (element, valueAccessor, allBindings) {
+
+		if (!window.Prism) {
+			throw 'Prism not loaded.';
+		}
+
+		// this should only be applied to a code element.
+		if (!new RegExp('^code$', 'i').test(element.tagName)) {
+			throw 'Please bind to a "code" element';
+		}
+
+		// check if we specified a language.
+		var has = allBindings.has('language');
+		console.log('has', has);
+		var language = allBindings.get('language');
+		if (language) {
+			element.className += language;
+		}
+
+	},
+	update: function (element, valueAccessor) {
+		element.innerHTML = ko.unwrap(valueAccessor());
+		window.Prism.highlightElement(element); // make prisim highlight the code again
+	}
+
+};
